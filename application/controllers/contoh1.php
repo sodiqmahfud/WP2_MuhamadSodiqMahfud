6d737923779380825d10f25993f64a9548dd4d32
sodiqmahfud <sodiq.mahfud@gmail.com>
2021-09-20 20:15:34 +0700

    tugas1

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,8 +4,9 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama Saya Muhamad Sodiq Mahfud
-             Saya tinggal di daerah Rawamangun
+        echo"Nama Saya Muhamad Sodiq Mahfud <br>
+             Saya tinggal di daerah Rawamangun <br>
              Olah Raga yang Saya Sukai adalah SepakBola";
     }
 }
+?>
